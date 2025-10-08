d534b5e0550f9a627c01bc6228b19ab97c572216
Taskeren <r0yalist@outlook.com>
2025-10-08 15:46:50 +0800

    Fixed subtitle of quest in Quest Button is not transformed

--- src/main/java/cn/elytra/mod/forcibly_translated/mixin/MixinQuestButton.java
+++ src/main/java/cn/elytra/mod/forcibly_translated/mixin/MixinQuestButton.java
@@ -11,11 +11,23 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(value = QuestButton.class, remap = false)
 public class MixinQuestButton {
 
-    @Redirect(method = "<init>", at = @At(
+    @Redirect(
+        method = "<init>", at = @At(
         value = "INVOKE",
         target = "Ldev/ftb/mods/ftbquests/quest/Quest;getTitle()Lnet/minecraft/network/chat/Component;"))
     private static Component forcibly_translated$redirectGetTitle(Quest instance) {
         return ForciblyTranslated.tryGetComponent(ForciblyTranslated.keyForQuestTitle(instance), instance::getTitle);
+    }
+
+    @Redirect(
+        method = "addMouseOverText",
+        at = @At(
+            value = "INVOKE",
+            target = "Ldev/ftb/mods/ftbquests/quest/Quest;getSubtitle()Lnet/minecraft/network/chat/Component;"))
+    private Component forcibly_translated$redirectGetSubtitle(Quest instance) {
+        return ForciblyTranslated.tryGetComponent(
+            ForciblyTranslated.keyForQuestSubtitle(instance),
+            instance::getSubtitle);
     }
 
 }
